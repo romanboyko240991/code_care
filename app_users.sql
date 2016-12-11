0c6c3407022ee0ef40d5d933154df20acce877f1
romanboyko240991 <romanboyko240991.work@gmail.com>
2016-12-11 02:40:54 +0200

    SQL for adding user with ROLE_ADMIN

---
+++ app_users.sql
@@ -0,0 +1,2 @@
+INSERT INTO `app_users` (`id`, `username`, `password`, `email`, `is_active`, `is_admin`) VALUES
+(1, 'admin', '$2y$10$NF/giLvr1TaeoltGD0PH7uhVWUqD5YogHmhE4BGB.KDDJWuT8M7q.', 'admin@test.local', 1, 1);
